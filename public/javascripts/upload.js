d2f5fbfbf2bf41bb7f875dfe114c8b2c2a0dfdf6
danielian1121 <danielian1121@gmail.com>
2018-06-29 11:30:03 +0800

    upload_required

--- public/javascripts/upload.js
+++ public/javascripts/upload.js
@@ -1,19 +1,22 @@
 
 
 function check_upload() {
-    var name = document.getElementById("name");
-    var price = document.getElementById("price");
-    var amount = document.getElementById("amount");
-    var itemInfo = document.getElementById("itemInfo");
-    var home = document.getElementById("home");
-    var foods = document.getElementById("foods");
-    var clothes = document.getElementById("clothes");
-    var ccc = document.getElementById("3C");
-    var pets = document.getElementById("pets");
-    var hashtag = document.getElementById("hashtag");
-    var ke_ent = document.getElementById("kf-ent");
-    var kf_cor = document.getElementById("kf-cor");
-    var lib = document.getElementById("lib");
-    var kf_dong = document.getElementById("kf-dong");
-    var other = document.getElementById("other");
+    var name = document.getElementById("name").value;
+    var price = document.getElementById("price").value;
+    var amount = document.getElementById("amount").value;
+    var itemInfo = document.getElementById("itemInfo").value;
+    var form1 = document.getElementsByName("form1");
+    var category;
+    for (var i = 0; i < form1.name.length; i++) {
+        if (form1.name[i].checked) {
+            category = form1.name[i].value;
+            break;
+        }
+    }
+    var hashtag = document.getElementById("hashtag").value;
+    var ke_ent = document.getElementById("kf-ent").value;
+    var kf_cor = document.getElementById("kf-cor").value;
+    var lib = document.getElementById("lib").value;
+    var kf_dong = document.getElementById("kf-dong").value;
+    var other = document.getElementById("other").value;
 }
